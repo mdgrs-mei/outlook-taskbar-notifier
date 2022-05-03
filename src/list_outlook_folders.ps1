a8129df929169c1aefd1158dc342b231fcf4c67b
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2022-05-03 21:08:12 +0900

    Implement UI for listing available folder paths

--- src/list_outlook_folders.ps1
+++ src/list_outlook_folders.ps1
@@ -1,8 +1,42 @@
-$outlook = New-Object -ComObject Outlook.Application
-if (-not $outlook.Name)
+Add-Type -AssemblyName System.Windows.Forms
+
+function CreateUi()
 {
-    "Failed to open Outlook."
-    return
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "Available Folder Paths"
+    $form.Width = 400
+    $form.Height = 400
+    $form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+
+    $table = New-Object System.Windows.Forms.TableLayoutPanel
+    $table.ColumnCount = 1
+    $table.RowCount = 2
+    $table.Dock = "Fill"
+    $form.Controls.Add($table)
+
+    $listBox = New-Object System.Windows.Forms.ListBox
+    $listBox.Dock = "Fill"
+    $listBox.IntegralHeight = $false
+    $listbox.Add_SelectedValueChanged({$button.Text = "Copy Path to Clipboard"})
+    $table.Controls.Add($listBox, 0, 0)
+    $table.RowStyles.Add((New-Object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 100))) | Out-Null
+
+    $button = New-Object System.Windows.Forms.Button
+    $button.Text = "Copy Path to Clipboard"
+    $button.Anchor = "Left, Right"
+    $button.Margin = 10
+    $button.BackColor = [System.Drawing.Color]::DarkSeaGreen
+    $button.Add_Click({
+        Set-Clipboard -Value ('"{0}"' -f $listBox.SelectedItem)
+        if ($listBox.SelectedItem)
+        {
+            $button.Text = "Copied!"
+        }
+    })
+    $table.Controls.Add($button, 1, 0)
+    $table.RowStyles.Add((New-Object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::AutoSize))) | Out-Null
+
+    $form, $listBox, $button
 }
 
 function IsTemporaryFolder($folder)
@@ -10,20 +44,29 @@ function IsTemporaryFolder($folder)
     return $folder.Name.Contains("MS-OLK")
 }
 
-function PrintFolders($folders)
+function AddToListBox($folders)
 {
     foreach ($folder in $folders)
     {
         if (-not (IsTemporaryFolder $folder))
         {
-            $folder.FolderPath
-            PrintFolders $folder.Folders
+            $listBox.Items.Add($folder.FolderPath) | out-null
+            AddToListBox $folder.Folders
         }
     }
 }
 
+$form, $listBox, $button = CreateUi
+
+$outlook = New-Object -ComObject Outlook.Application
+if (-not $outlook.Name)
+{
+    Read-Host "Failed to open Outlook. Press any key to close"
+    return
+}
+
 $namespace = $outlook.GetNamespace("MAPI")
-PrintFolders $namespace.Folders
+AddToListBox $namespace.Folders
 
 foreach ($store in $namespace.Stores)
 {
@@ -37,7 +80,10 @@ foreach ($store in $namespace.Stores)
         Write-Host $PSItem
         continue
     }
-    PrintFolders $searchFolders
+    AddToListBox $searchFolders
 }
 
+$form.ShowDialog() | Out-Null
+
 [system.runtime.interopservices.marshal]::releasecomobject($outlook) | Out-Null
+
--- tools/list_outlook_folders.bat
+++ tools/list_outlook_folders.bat
@@ -1,4 +1,3 @@
 @echo off
 cd %~dp0
 powershell.exe -ExecutionPolicy Bypass "..\src\list_outlook_folders.ps1"
-pause
